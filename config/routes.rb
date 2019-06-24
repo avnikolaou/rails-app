e8db9a10b1423cf62bc494374c973e58c8de305d
Avraam Nikolaou <avnikolaou@avlos.io>
2019-06-24 12:51:19 +0300

    fix: customize portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :blogs
+
+  # Override default resources routes and create a custom
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # Customize routes
   get '/about-me', to: 'pages#about'
@@ -8,4 +10,6 @@ Rails.application.routes.draw do
 
   # Set the root page
   root to: 'pages#home'
+
+  resources :blogs
 end

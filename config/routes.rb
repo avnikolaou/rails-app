04e73b25c5648b50d658e7286ceaf26e703ece35
Avraam Nikolaou <avnikolaou@avlos.io>
2019-06-26 19:58:02 +0300

    fix: customize devise defualt routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # Override default resources routes and create a custom
 
   resources :portfolios, except: [:show]

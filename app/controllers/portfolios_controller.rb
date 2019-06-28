b4092610d5ffeca62425f500dbcb82e1357ab4c8
Avraam Nikolaou <avnikolaou@avlos.io>
2019-06-28 10:22:23 +0300

    fix: update portfolio to include postion attr

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   layout "portfolio"
   def index
-    @portfolio_item = Portfolio.all
+    @portfolio_item = Portfolio.by_position
   end
 
   def angular
